7585ac8f68ef9c851064933d55f5140c1dba4113
LucasTecDev <lucasplussoft@gmail.com>
2022-06-19 23:35:06 -0300

    Caixa_Mensagem
    Finalizada programação de hoje com MessageBox no Delphi

---
+++ Caixa_Mensagem.dpr
@@ -0,0 +1,14 @@
+program Caixa_Mensagem;
+
+uses
+  Vcl.Forms,
+  uPrincipal in 'uPrincipal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
